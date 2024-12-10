de67ba98b58c4ea6382205fb4c7fe600ff986094
Wong Jia XIn <jiaxin.wong.2023@scis.smu.edu.sg>
2024-12-10 10:15:05 +0800

    update navigation menu

--- .history/heartcode/app/page_20241209174954.tsx
+++ .history/heartcode/app/page_20241209174954.tsx
@@ -2,7 +2,7 @@
  
 import * as React from "react"
 // import Link from "next/link"
-import { NavigationMenuDemo } from "../components/navbar/navigation-menu"
+import { NavigationMenuDemo } from "./components/navbar/navigation-menu"
 
 
 export default function Home() {
